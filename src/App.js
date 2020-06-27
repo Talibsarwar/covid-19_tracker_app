6443a9061171514ae83800b88c928e92453e5faf
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-06-27 01:45:17 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Columns from 'react-columns';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 function App() {
+  const [latest, setLatest] = useState([]);
+  const [results, setResults] = useState([])
+  const [searchCountries, setsearchCountries] = useState("");
+  useEffect(() => {
+    axios
+      .all([
+        axios.get("https://corona.lmao.ninja/v2/all"),
+        axios.get("https://corona.lmao.ninja/v2/countries")
+      ])
+      .then(responseArr => {
+        setLatest(responseArr[0].data);
+        setResults(responseArr[1].data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const date = new Date(parseInt(latest.updated));
+  const lastUpdated = date.toString();
+
+  const filterCountries = results.filter(item => {
+    return searchCountries !== "" ? item.country.toLowerCase().includes(searchCountries.toLowerCase()) : item;
+  })
+
+  const countries = filterCountries.map((data, i) => {
+    return (
+      <Card
+        key={i}
+        bg="light"
+        text="dark"
+        className="text-center"
+        style={{ margin: "10px" }}
+      >
+        <Card.Img variant="top" src={data.countryInfo.flag} />
+        <Card.Body>
+          <Card.Title>{data.country}</Card.Title>
+          <Card.Text>Cases: {data.cases}</Card.Text>
+          <Card.Text>Deaths: {data.deaths}</Card.Text>
+          <Card.Text>Recovered: {data.recovered}</Card.Text>
+          <Card.Text>Today Cases: {data.todayCases}</Card.Text>
+          <Card.Text>Today Deaths: {data.todayDeaths}</Card.Text>
+          <Card.Text>Active: {data.active}</Card.Text>
+          <Card.Text>Critical: {data.critical}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  })
+
+  var queries = [{
+    columns: 2,
+    query: 'min-width: 500px'
+  }, {
+    columns: 3,
+    query: 'min-width: 1000px'
+  }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="Rights">COVID-19 TRACKER APP By Talib Sarwar</h1>
+      <CardDeck>
+        <Card bg="secondary" text="white" className="text-center" style={{ margin: "10px" }}>
+          <Card.Body>
+            <Card.Title>Cases</Card.Title>
+            <Card.Text>{latest.cases}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small> Last Updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="danger" text="white" className="text-center" style={{ margin: "10px" }}>
+          <Card.Body>
+            <Card.Title>Deaths</Card.Title>
+            <Card.Text>{latest.deaths}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small> Last Updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="success" text="white" className="text-center" style={{ margin: "10px" }}>
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>{latest.recovered}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small> Last Updated {lastUpdated}</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+      <br />
+      <Form>
+        <Form.Group controlId="formGroupSearch">
+          <Form.Control type="text" placeholder="Search a country"
+            onChange={e => setsearchCountries(e.target.value)}
+          />
+        </Form.Group>
+      </Form>
+      <Columns queries={queries}>{countries}</Columns>
     </div>
   );
 }
