12b35f512d277b910ad64efcd6d1590492e3a259
Talibsarwar <47272482+Talibsarwar@users.noreply.github.com>
2020-06-30 08:50:19 +0000

    Update

--- src/pages/Graph.js
+++ src/pages/Graph.js
@@ -1,85 +1,41 @@
 import React from "react";
-import "../App.css";
-import ReactApexChart from "react-apexcharts";
+import { Chart, CountryPicker, Cards } from '../components';
+import styles from "../App.css";
+import { fetchData } from '../api';
 
-function Graph() {
-  const series = [
-    {
-      name: "Cases",
-      data: [
-        555,
-        12038,
-        69030,
-        88369,
-        167466,
-        932638,
-        2055423,
-        3343777,
-        3845718,
-      ],
-    },
-    {
-      name: "Recovered",
-      data: [28, 284, 9394, 42710, 76026, 191853, 501538, 1029651, 1255481],
-    },
-    {
-      name: "Deaths",
-      data: [17, 259, 1666, 2996, 6472, 49675, 140658, 238619, 269567],
-    },
-  ];
-  const options = {
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      curve: "smooth",
-    },
-    xaxis: {
-      type: "datetime",
-      categories: [
-        "1/22/20",
-        "2/1/20",
-        "2/15/20",
-        "3/1/20",
-        "3/15/20",
-        "4/1/20",
-        "4/15/20",
-        "5/1/20",
-        "5/7/20",
-      ],
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy",
-      },
-    },
-  };
+class Graph extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-  return (
-    <div
-      style={{
-        backgroundColor: "white",
-        textAlign: "center",
-      }}
-    >
-      <br />
-      <h2>COVID-19 Global Graphs</h2>
-      <br />
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="area"
-        height={350}
-      />
-      <br />
-      <ReactApexChart
-        options={options}
-        series={series}
-        type="bar"
-        height={350}
-      />
-    </div>
-  );
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1>COVID-19 TRACKER APP By Talib Sarwar</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <div classsName="filter">
+        </div>
+        <br />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
 }
 
 export default Graph;
